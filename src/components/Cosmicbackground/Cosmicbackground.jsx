325ba6bbf5404ae2b0f2a38fddf0fd9eff6442c8
Namit Kumar Krrish <199653552+Namitkumarkrrish@users.noreply.github.com>
2026-03-05 20:16:02 +0530

    deployement issue fixed

--- src/components/Cosmicbackground/Cosmicbackground.jsx
+++ src/components/Cosmicbackground/Cosmicbackground.jsx
@@ -3,7 +3,7 @@ import { Canvas, useFrame } from '@react-three/fiber';
 import { Points, PointMaterial, Float, Line } from '@react-three/drei';
 import * as random from 'maath/random/dist/maath-random.esm';
 import * as THREE from 'three';
-import styles from './CosmicBackground.module.css';
+import styles from './Cosmicbackground.module.css';
 
 const Constellation = ({ points, position, scale = 1 }) => {
   return (
